170827e1b1ef07a0f071c15311f5523839e13761
rahul <shanrahul03@gmail.com>
2025-02-02 07:20:52 +0530

    commit

--- src/Pages/Market.tsx
+++ src/Pages/Market.tsx
@@ -1,26 +1,28 @@
-// Pages/Market.tsx
-
 import SwapComponent from '../Components/Swap';
 import WalletComponent from '../Components/Wallet';
+import '../App.css';  // Import global styles
 import BuyComponent from '../Components/Buy';
 
-function Market(){
+
+const MarketPage = () => {
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen bg-gray-100 p-8">
-     
-      <h1 className="text-2xl font-bold mb-8 text-center">Crypto Transactions</h1>
+    <>
+      {/* Wallet Component - Top Right */}
       <div className="w-full flex justify-end mb-8">
         <WalletComponent />
       </div>
+
+      {/* Swap Component - Centered */}
       <div className="w-full flex justify-center mb-8">
         <SwapComponent />
       </div>
-      <hr></hr>
-      <div className="w-full flex justify-center mt-3">
+
+      <div className="w-full flex justify-center mb-8">
         <BuyComponent />
       </div>
-    </div>
+
+    </>
   );
 };
 
-export default Market;
+export default MarketPage;
